14c6dee390ccdb229b1133c909077ce40dcbc948
Harish <harishtella@gmail.com>
2011-06-28 01:40:12 -0500

    add integrations test and fix other tests

--- test/functional/playlist_controller_test.rb
+++ test/functional/playlist_controller_test.rb
@@ -12,21 +12,26 @@ class PlaylistControllerTest < ActionController::TestCase
       :min_rating => '2'
     assert_response :success
     assert_not_nil assigns(:mp3s)
-    assert_equal(@response.content_type, 'audio/x-mpegurl')
+    assert_equal(@response.headers['Content-Type'], 'audio/x-mpegurl')
+    assert_equal(@response.headers['Content-Disposition'], 
+                 'attachment; filename=playlist.m3u')
   end
 
-  test "should generate playlist also" do
+  test "should generate playlist as well" do
     post :generate, :artists => nil,
       :min_rating => '-1'
     assert_response :success
     assert_not_nil assigns(:mp3s)
-    assert_equal(@response.content_type, 'audio/x-mpegurl')
+    assert_equal(@response.headers['Content-Type'], 'audio/x-mpegurl')
+    assert_equal(@response.headers['Content-Disposition'], 
+                 'attachment; filename=playlist.m3u')
   end
 
   test "when no songs match criteria" do
     post :generate, :artists => ["Beethoven"],
       :min_rating => '5'
     assert_redirected_to '/playlist/index'
+    assert_equal(@response.content_type, 'text/html')
     assert_not_nil assigns(:mp3s)
     assert_not_nil flash[:error]
   end
